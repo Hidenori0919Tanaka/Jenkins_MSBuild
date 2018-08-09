ae37dc2836dbffbfcf2628c6f4c8301612ef1042
田中 秀典 <hidenori091985@yahoo.co.jp>
2018-08-09 15:51:23 +0900

    Initial Commit

---
+++ ConsoleApp/ConsoleAppTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
